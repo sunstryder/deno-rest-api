bc5adf3ead4f14ea05b12e3a398290a4e705d43a
Tony Sun <tsun@zendesk.com>
2021-03-02 10:48:32 +1100

    add logic for addProduct route

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,4 +1,5 @@
 import { Product } from '../types.ts'
+import {v4} from 'https://deno.land/std/uuid/mod.ts'
 
 let products:Product[] = [
     { id: '1', name: 'product 1', description: 'Product description', price: 30.88 },
@@ -35,8 +36,26 @@ const getSingleProduct= ({params, response}:{params: {id:string}, response:any }
     }
 }
 
-const addProduct= ({response}:{response:any }) => {
-    response.body = 'add'
+const addProduct = async ({ request, response }: { request: any, response: any }) => {
+    const body = await request.body()
+    console.log("🚀 ~ file: products.ts ~ line 41 ~ addProduct ~ body", body)
+    if (!request.hasBody) {
+        response.status = 400
+        response.body = {
+            success: false,
+            msg: 'No data in request'
+        }
+    } else {
+        const product: Product= body.value
+        console.log("🚀 ~ file: products.ts ~ line 50 ~ addProduct ~ product", product)
+        product.id = v4.generate()
+        products.push(product)
+        response.status = 201
+        response.body = {
+            success: true,
+            data:product
+        }
+    }
 }
 
 const updateProduct= ({response}:{response:any }) => {
